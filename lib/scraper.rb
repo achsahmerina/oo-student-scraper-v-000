38ca3d28f992a77c871200efe200f402ab8dabca
Achsah Abraham <achsahmp@gmail.com>
2018-10-18 01:05:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,10 +18,9 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    
 
     student = {}
-    profile_page = Nokogiri::HTML(open(profile_slug))
+    profile_page = Nokogiri::HTML(open(profile_url))
     links = profile_page.css(".social-icon-container").children.css("a").map { |elm| elm.attribute('href').value}
     links.each do |link|
       if link.include?("linkedin")
