e9bc6c00bd849e3498e927a097bef86d8e7fa2a2
Achsah Abraham <achsahmp@gmail.com>
2018-10-18 01:16:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,8 +18,8 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |attr, value|
-      self.send("#{attr}=", value)
+    attributes_hash.each do |attr, v|
+      self.send("#{attr}=", v)
     end
     self
   end
